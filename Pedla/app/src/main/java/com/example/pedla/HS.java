6a641a72621d670cd3abea39737b998495fab3d7
Muskan Paliwal <muskan10112002@gmail.com>
2020-10-08 09:35:57 +0530

    ui: created preferences and settings file for settings

--- Pedla/app/src/main/java/com/example/pedla/HS.java
+++ Pedla/app/src/main/java/com/example/pedla/HS.java
@@ -1,9 +1,12 @@
 package com.example.pedla;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -26,10 +29,25 @@ public class HS extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.setp:
+                Intent i = new Intent(HS.this, Preference.class);
+                startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
     private void backToSignInpage(){
         Intent intent=new Intent (HS.this,Login.class);
         startActivity(intent);
         finish();
     }
+
 }
---
+++ Pedla/app/src/main/java/com/example/pedla/Preference.java
@@ -0,0 +1,33 @@
+package com.example.pedla;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.PreferenceManager;
+
+public class Preference extends PreferenceActivity{
+    @Override
+    protected void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+    }
+
+    private void load_setting(){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean check_night = sp.getBoolean("NIGHT", false);
+        if(check_night){
+            getListView().setBackgroundColor(Color.parseColor("#222222"));
+        } else{
+            getListView().setBackgroundColor(Color.parseColor("#ffffff"));
+        }
+
+        CheckBoxPreference check_night_instant = (CheckBoxPreference)findPreference("NIGHT");
+
+    }
+}
