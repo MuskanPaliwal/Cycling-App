fe1242d1ebba1af5a8b2958967861184b1e4f044
JinalGandhi07 <gandhi07@gmail.com>
2020-10-11 18:32:52 +0530

    Added Navigation bar to all activities

--- Pedla/app/src/main/java/com/example/pedla/HS.java
+++ Pedla/app/src/main/java/com/example/pedla/HS.java
@@ -1,12 +1,19 @@
 package com.example.pedla;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HS extends AppCompatActivity {
     private Button btnbacktologin;
@@ -15,7 +22,7 @@ public class HS extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_h_s);
+        setContentView(R.layout.nav_activity_h_s);
 
 
         btnbacktologin=findViewById(R.id.btn5);
@@ -26,6 +33,28 @@ public class HS extends AppCompatActivity {
                 backToSignInpage();
             }
         });
+
+        NavigationView navigationView=findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            public boolean onNavigationItemSelected(MenuItem item){
+                if(item.getItemId()==R.id.nav_home){
+                    Intent intent=new Intent(HS.this,SideBar.class);
+                    startActivity(intent);
+                }
+                else if(item.getItemId()==R.id.nav_settings){
+                    Intent intent=new Intent(HS.this,HS.class);
+                    startActivity(intent);
+                }
+                else if(item.getItemId()==R.id.nav_aboutus){
+
+                }
+                DrawerLayout drawerLayout=findViewById(R.id.drawer_layout);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+
+            }
+        });
+
     }
 
     private void backToSignInpage(){
@@ -33,4 +62,7 @@ public class HS extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+
+
 }
