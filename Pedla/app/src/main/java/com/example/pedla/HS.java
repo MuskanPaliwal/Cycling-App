56338b81d2287ff489d0442aadb62883be815faf
Muskan Paliwal <muskan10112002@gmail.com>
2020-11-25 21:04:05 +0530

    Revert "ui: BookSuccessfull and Payment screens"

--- Pedla/app/src/main/java/com/example/pedla/HS.java
+++ Pedla/app/src/main/java/com/example/pedla/HS.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationView;
 
