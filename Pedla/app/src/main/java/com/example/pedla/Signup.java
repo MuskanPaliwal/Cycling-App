b2ca18278bac029644845e21a24d2cf692efa275
Muskan Paliwal <muskan10112002@gmail.com>
2020-12-09 13:00:07 +0530

    backend: connected firebase to the application and implemented signup authentication

--- Pedla/app/src/main/java/com/example/pedla/Signup.java
+++ Pedla/app/src/main/java/com/example/pedla/Signup.java
@@ -1,8 +1,10 @@
 package com.example.pedla;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,10 +14,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Signup extends AppCompatActivity {
     private Button btnmovetologin;
     private Button btnmovetoCHOICE;
     private EditText Name, Email, Password, Phone,ConfirmPass;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,7 @@ public class Signup extends AppCompatActivity {
         Password=(EditText)findViewById(R.id.editTextTextPassword);
         Phone=(EditText)findViewById(R.id.editTextTextPersonName);
         ConfirmPass=(EditText)findViewById(R.id.editTextTextPasswordType) ;
+        fAuth = FirebaseAuth.getInstance();
 
 
         btnmovetologin=findViewById(R.id.btn3);
@@ -43,11 +52,23 @@ public class Signup extends AppCompatActivity {
         btnmovetoCHOICE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
                 if (moveTohomescreen()) {
-                    Intent intent=new Intent (Signup.this,SideBar.class);
-                    startActivity(intent);
-                    finish();
+                    //Signup user in Firebase
+                    fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(Signup.this, "Account Created", Toast.LENGTH_SHORT).show();
+                                Intent intent=new Intent (Signup.this,SideBar.class);
+                                startActivity(intent);
+                                finish();
+                            } else{
+                                Toast.makeText(Signup.this, "Error!" + task.getException(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
                 else{
                     AlertDialog();
@@ -64,40 +85,47 @@ public class Signup extends AppCompatActivity {
         finish();
     }
 
-    private boolean moveTohomescreen(){
+    private boolean moveTohomescreen()
+    {
         String MobilePattern = "[0-9]{10}";
         //String email1 = email.getText().toString().trim();
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        String email = Email.getText().toString().trim();
+        String password = Password.getText().toString().trim();
+        String phoneNumber = Phone.getText().toString().trim();
 
         if (Name.length() > 25) {
 
             Toast.makeText(getApplicationContext(), "Please enter less than 25 character in user name", Toast.LENGTH_SHORT).show();
             return false;
 
-        } else if (Name.length() == 0 || Phone.length() == 0 || Email.length() ==
+        }
+        if (Name.length() == 0 || Phone.length() == 0 || Email.length() ==
                 0 || Password.length() == 0 ||ConfirmPass.length()==0) {
 
             Toast.makeText(getApplicationContext(), "Please fill the empty fields", Toast.LENGTH_SHORT).show();
             return false;
 
-        }  else if(!Email.getText().toString().matches(emailPattern)) {
+        }
+        if(!email.matches(emailPattern)) {
 
             Toast.makeText(getApplicationContext(),"Please Enter Valid Email Address",Toast.LENGTH_SHORT).show();
             return false;
 
-        } else if(!Phone.getText().toString().matches(MobilePattern)) {
+        }
+        if(!phoneNumber.matches(MobilePattern)) {
 
             Toast.makeText(getApplicationContext(), "Phone number is invalid", Toast.LENGTH_SHORT).show();
             return false;
         }
-        else if(!Password.getText().toString().equals(ConfirmPass.getText().toString())){
+        if(!password.equals(ConfirmPass.getText().toString())){
             Toast.makeText(getApplicationContext(), "Password not matching", Toast.LENGTH_SHORT).show();
             return false;
-        } else if (Password.length() < 6) {
+        }
+        if (Password.length() < 6) {
             Toast.makeText(getApplicationContext(), "Please enter more than 6 characters in password", Toast.LENGTH_SHORT).show();
             return false;
         }
-
         return true;
     }
     private void AlertDialog()
