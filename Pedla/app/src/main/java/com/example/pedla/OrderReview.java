980e6956cb4a57f4426dc93e378c5b0a095ef059
Muskan Paliwal <muskan10112002@gmail.com>
2020-12-13 22:31:32 +0530

    backend: added functionality to save user's bookings

--- Pedla/app/src/main/java/com/example/pedla/OrderReview.java
+++ Pedla/app/src/main/java/com/example/pedla/OrderReview.java
@@ -1,12 +1,12 @@
 package com.example.pedla;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderReview extends AppCompatActivity {
     private Button btnToPayAmount;
