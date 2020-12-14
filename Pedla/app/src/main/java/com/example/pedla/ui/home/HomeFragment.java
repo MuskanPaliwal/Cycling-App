5ce9f25bdde8e4a074a4892d0013267ca7df58cb
JinalGandhi07 <gandhi07@gmail.com>
2020-12-14 11:47:05 +0530

    Backend:Bookings image savde and fetched, UI:homescreen edittext added

--- Pedla/app/src/main/java/com/example/pedla/ui/home/HomeFragment.java
+++ Pedla/app/src/main/java/com/example/pedla/ui/home/HomeFragment.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,25 +27,61 @@ import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
 import com.example.pedla.AboutApp;
-import com.example.pedla.BadiLake;
-import com.example.pedla.BookingSuccessfull;
 import com.example.pedla.Bookings;
 import com.example.pedla.CovidRules;
-import com.example.pedla.DoodhTalayi;
-import com.example.pedla.GovardhanSagar;
-import com.example.pedla.OldCity;
 import com.example.pedla.R;
-import com.example.pedla.RaniRoad;
-import com.example.pedla.SajjanGarh;
+import com.example.pedla.Signup;
 import com.example.pedla.Stores;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Calendar;
 
+/*import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.pedla.HS;
+*/
+
+/*import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+*/
+
+import com.example.pedla.AboutApp;
+import com.example.pedla.BadiLake;
+import com.example.pedla.Choice;
+import com.example.pedla.CovidRules;
+
+import com.example.pedla.DoodhTalayi;
+import com.example.pedla.GovardhanSagar;
+import com.example.pedla.Login;
+import com.example.pedla.OldCity;
+import com.example.pedla.R;
+/*import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+*/import com.example.pedla.RaniRoad;
+import com.example.pedla.SajjanGarh;
+import com.example.pedla.Stores;
+import com.google.android.gms.common.api.Status;
+
+/*import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;*/
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+//import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+/*import static android.app.Activity.RESULT_OK;
+>>>>>>> 3c0ae0b0ad82fd48ac272d76637360a1523d203f
+*/
 public class HomeFragment extends Fragment  {
     TextView profilename;
     FirebaseAuth fAuth;
@@ -54,6 +89,7 @@ public class HomeFragment extends Fragment  {
     String Userid;
 
     private CardView cardView1;
+    private CardView cardView3;
     ImageView img1;
     ImageView img2;
     ImageView img3;
@@ -61,7 +97,7 @@ public class HomeFragment extends Fragment  {
     ImageView img5;
     ImageView img6;
 
-    private CardView cardView2,cardview3;
+    private CardView cardView2;
     private HomeViewModel homeViewModel;
     LinearLayout ml;
     private TextView t1;
@@ -74,10 +110,13 @@ public class HomeFragment extends Fragment  {
     TimePickerDialog timePickerDialog;
     DatePickerDialog.OnDateSetListener setListener;
     String amPm;
-    TextView textView1;
+    EditText editText;
+   
     private Button btntostore;
-
-
+    /* Button btnpicker;
+     TextView location;
+     TextView name,longitude;
+     int PLACE_PICKER_REQUEST=1;*/
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -94,6 +133,7 @@ public class HomeFragment extends Fragment  {
         });*/
 
 
+
         img1=root.findViewById(R.id.cimg1);
         img2=root.findViewById(R.id.cimg2);
         img3=root.findViewById(R.id.cimg3);
@@ -108,7 +148,7 @@ public class HomeFragment extends Fragment  {
 
         cardView1=root.findViewById(R.id.card11);
         cardView2=root.findViewById(R.id.card22);
-        cardview3=root.findViewById(R.id.card33);
+        cardView3=root.findViewById(R.id.card33);
 
         tvdate=root.findViewById(R.id.tv_date);
         etdate=root.findViewById(R.id.et_date);
@@ -118,8 +158,14 @@ public class HomeFragment extends Fragment  {
         tvtime=root.findViewById(R.id.tv_time);
         ettime2=root.findViewById(R.id.et_time2);
 
+        editText=root.findViewById(R.id.edit_text);
+
 
         btntostore=root.findViewById(R.id.btnstores);
+       /* btnpicker=root.findViewById(R.id.picker);
+        location=root.findViewById(R.id.address);
+        name=root.findViewById(R.id.name);
+        longitude=root.findViewById(R.id.longitude);*/
 
         final Calendar calender=Calendar.getInstance();
         final int year= calender.get(Calendar.YEAR);
@@ -140,8 +186,6 @@ public class HomeFragment extends Fragment  {
         final String[] time1 = new String[1];
         final String[] time2=new String[1];
 
-
-      
         DocumentReference documentReference=fStore.collection("Users").document(Userid);
 
         documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -214,17 +258,35 @@ public class HomeFragment extends Fragment  {
                 startActivity(intent);
             }
         });
-
-        cardview3.setOnClickListener(new View.OnClickListener(){
+        cardView3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent=new Intent(HomeFragment.this.getActivity(), Bookings.class);
                 startActivity(intent);
             }
         });
+/*
+
+Places.initialize(HomeFragment.this.getActivity(),"AIzaSyAIrloRRV7osj8QUUa3KfuwzRwzPvVP35A");
+=======
+        //Places.initialize(HomeFragment.this.getActivity(),"AIzaSyAIrloRRV7osj8QUUa3KfuwzRwzPvVP35A");
+>>>>>>> 3c0ae0b0ad82fd48ac272d76637360a1523d203f
+
+        editText.setFocusable(false);
+
+        /*editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Place.Field> fieldList= Arrays.asList(Place.Field.ADDRESS,
+                        Place.Field.NAME);
+                Intent intent=new Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN,
+                        fieldList).build(HomeFragment.this.getActivity());
+
+                startActivityForResult(intent,100);
+            }
+        });*/
 
 
-      
         btntostore.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -242,7 +304,6 @@ public class HomeFragment extends Fragment  {
                 }
             }
         });
-
 
         etdate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -326,12 +387,44 @@ public class HomeFragment extends Fragment  {
             }
         });
 
+        /* btnpicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder=new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(HomeFragment.this.getActivity())
+                    ,PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+            }
+        });*/
 
         LoadSettings();
 
         return root;
 
     }
+   /* @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==100 && resultCode==RESULT_OK){
+            Place place=Autocomplete.getPlaceFromIntent(data);
+            editText.setText(place.getAddress());
+
+            textView1.setText(String.format("Locality Name: %s",place.getName()));
+
+
+
+        }else if(requestCode== AutocompleteActivity.RESULT_ERROR){
+            Status status = Autocomplete.getStatusFromIntent(data);
+            Toast.makeText(HomeFragment.this.getActivity(),status.getStatusMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }*/
+
 
     private boolean moveToStores(){
         String dateone, datetwo,timeone,timetwo;
@@ -375,7 +468,7 @@ public class HomeFragment extends Fragment  {
             t1.setTextColor(Color.parseColor("#ffffff"));
             tvtime.setTextColor(Color.parseColor("#ffffff"));
             tvdate.setTextColor(Color.parseColor("#ffffff"));
-            textView1.setTextColor(Color.parseColor("#ffffff"));
+            profilename.setTextColor(Color.parseColor("#ffffff"));
 
         }
         else{
@@ -383,7 +476,7 @@ public class HomeFragment extends Fragment  {
             t1.setTextColor(Color.parseColor("#000000"));
             tvtime.setTextColor(Color.parseColor("#000000"));
             tvdate.setTextColor(Color.parseColor("#000000"));
-            textView1.setTextColor(Color.parseColor("#000000"));
+            profilename.setTextColor(Color.parseColor("#000000"));
         }
 
 
@@ -409,4 +502,20 @@ public class HomeFragment extends Fragment  {
         super.onResume();
     }
 
+/*@Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==PLACE_PICKER_REQUEST){
+            if (resultCode==RESULT_OK){
+                Place place= PlacePicker.getPlace(data,HomeFragment.this.getActivity());
+
+                String Location = String.format(String.valueOf(place.getName()));
+                String addr=String.format(String.valueOf( place.getAddress()));
+                String latitude=String.valueOf(place.getLatLng().longitude);
+
+                location.setText(Location);
+                name.setText(addr);
+                longitude.setText(latitude);
+            }
+        }
+    }*/
 }
